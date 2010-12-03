3d0ff7834ea0990e08fe19fccda620b5d354e8d5
Jesse Lang <jesse@jesselang.com>
2010-12-03 13:22:05 -0600

    A little more structure on the SCGI package.

--- solid-web-scgi.adb
+++ solid-web-scgi.adb
@@ -3,16 +3,31 @@ with GNAT.Sockets;
 
 package body Solid.Web.SCGI is
    task body Server is
+      Service_Socket : GNAT.Sockets.Socket_Type;
    begin -- Server
-      select
-         accept Bind;
-      or
+      -- Create
+      -- Set options
+
+      accept Bind;
+      -- Bind
+      -- Listen
+
+      Forever : loop
          accept Start;
 
-         -- Check CONTENT_LENGTH.  Must be present, even if "0".
-         -- Check SCGI.  Should be "1".
-      or
-         accept Stop;
-      end select;
+         Handle_Requests : loop
+            select
+               accept Stop;
+
+               exit Handle_Requests;
+            else
+               -- Use a selector to get a new socket with a request?
+
+               -- Check CONTENT_LENGTH.  Must be present, even if "0".
+               -- Check SCGI.  Should be "1".
+               null;
+            end select;
+         end loop Handle_Requests;
+      end loop Forever;
    end Server;
 end Solid.Web.SCGI;
--- solid-web-scgi.ads
+++ solid-web-scgi.ads
@@ -1,17 +1,26 @@
 -- SCGI support.
--- Initial implementation written by Luke A. Guest (_Lucretia_) in early 2010.
--- Adapted for use with Solid.Web in April 2010.
+-- Initial reference code written by Luke A. Guest (_Lucretia_) in early 2010.
+-- Adapted for use with Solid.Web in June 2010.
 
 with Solid.Web.Concurrency;
 
 package Solid.Web.SCGI is
+   Not_Bound : exception;
+
    task type Server (Port      : Network_Port;
                      IO_Tasks  : Concurrency.Task_Count;
                      Requests  : Concurrency.Request_Queue_Handle;
                      Responses : Concurrency.Response_Queue_Handle)
    is
       entry Bind;
+      -- Binds the server to Port.
+      -- Raises Not_Bound if the server fails to bind to Port.
+
       entry Start;
+      -- Begin accepting requests.
+      -- The server must be bound before it can be started.
+
       entry Stop;
+      --Stop accepting requests.
    end Server;
 end Solid.Web.SCGI;
